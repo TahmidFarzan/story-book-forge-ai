da3a3a143e48bd56a64a6ce34ca2f58d115c1729
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-09-21 10:13:55 +0600

    Update for status

--- src/resources/js/composables/useAiBrain.js
+++ src/resources/js/composables/useAiBrain.js
@@ -2,4 +2,3 @@ export const AiBrainOutputTypes = {
     Text: 'Text',
     Image: 'Image',
 }
-
---
+++ src/resources/js/composables/useStoryBook.js
@@ -0,0 +1,6 @@
+export const statuses = {
+    Draft: 'Draft',
+    Ongoing: 'Ongoing',
+    Pending: 'Pending',
+    Complete: 'Complete',
+}
